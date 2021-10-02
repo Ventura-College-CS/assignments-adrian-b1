ede5c84592436e79557dab385985a00df6da9d34
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-10-01 18:32:02 -0700

    Assignment 2-2

---
+++ Assignment2-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void printArray(double * const n, int N);
+int main()
+{
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+    double *ptr;
+
+    ptr=numbers;
+    printArray(ptr, N);
+    return 0;
+}
+void printArray(double * const n, int N){
+     for(int i=0;i<N;i++){
+        //*(n+i) instead of numbers[i] for pointer notation
+        cout<<*(n+i)<<"\t";
+    }
+    cout<<endl;
+}
