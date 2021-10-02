48a857e03f75265df0017e1a815b8d693bfbee39
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-10-01 18:24:13 -0700

    Assignment 2-1

---
+++ Assignment2-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+    double *ptr;
+
+    ptr=numbers;
+    for(int i=0;i<N;i++){
+        //*(ptr+i) instead of numbers[i] for pointer notation
+        cout<<*(ptr+i)<<"\t";
+    }
+    cout<<endl;
+    return 0;
+}
