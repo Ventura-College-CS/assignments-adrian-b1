8c80b10431abaff1c384f51ff60ead2141fcf149
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 22:17:46 -0800

    midterm q1 edit

--- Midterm/main-midterm-q1.cpp
+++ Midterm/main-midterm-q1.cpp
@@ -8,9 +8,10 @@ using namespace std;
 int main()
 {
 Stack<Student> students;
+//generate 5 student objects
 double g1[3]={95.0, 99.5, 90.0};
 double g2[3]={78.4, 81.4, 92.0};
-double g3[3]={77.4, 55.6, 84,2};
+double g3[3]={77.4, 55.6, 84.2};
 double g4[3]={88.1, 23.5, 46.0};
 double g5[3]={90.2, 80.1, 70.0};
 Student s1 (100, "John", g1);
@@ -18,9 +19,14 @@ Student s2 (101, "Johnny", g2);
 Student s3 (102, "Johnson", g3);
 Student s4 (103, "Johnathan", g4);
 Student s5 (104, "Jon", g5);
+//push student objects to the stack
 students.push(s1);
 students.push(s2);
 students.push(s3);
 students.push(s4);
 students.push(s5);
+//print size of the stack
+cout<<students.size()<<endl;
+//pop and print all student info until stack is empty
+
 }
