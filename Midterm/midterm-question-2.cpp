4c8178137549989c78d3ff4b40220c38f48ff47f
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 22:54:38 -0800

    edit so the students don't start in order

--- Midterm/midterm-question-2.cpp
+++ Midterm/midterm-question-2.cpp
@@ -22,8 +22,9 @@ class StudentCompare
 int main()
 {
     double g1[3]={95.0, 99.5, 90.0};
-    double g2[3]={78.4, 81.4, 92.0};
-    double g3[3]={77.4, 55.6, 84.2};
+    double g2[3]={77.4, 55.6, 84.2};
+    double g3[3]={78.4, 81.4, 92.0};
+    
     Student s1 (100, "John", g1);
     Student s2 (101, "Johnny", g2);
     Student s3 (102, "Johnson", g3);
