51c3961df7bdaecad13b14532298a85cfa525a58
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 21:05:30 -0800

    Midterm q1 progress

---
+++ Midterm/Student.hpp
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+    private:
+        int sID;
+        string sName;
+        double scores[3];
+    public:
+}
