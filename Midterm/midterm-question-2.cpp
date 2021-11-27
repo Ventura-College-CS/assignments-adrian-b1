a1f94741866fca965561cdb553f042c7766aaa2f
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 23:13:11 -0800

    midterm question 2 added two more students

--- Midterm/midterm-question-2.cpp
+++ Midterm/midterm-question-2.cpp
@@ -24,15 +24,19 @@ int main()
     double g1[3]={95.0, 99.5, 90.0};
     double g2[3]={77.4, 55.6, 84.2};
     double g3[3]={78.4, 81.4, 92.0};
-    
+    double g4[3]={88.1, 23.5, 46.0};
+    double g5[3]={90.2, 80.1, 70.0};
     Student s1 (100, "John", g1);
     Student s2 (101, "Johnny", g2);
     Student s3 (102, "Johnson", g3);
-
+    Student s4 (103, "Johnathan", g4);
+    Student s5 (104, "Jon", g5);
     priority_queue<Student, vector<Student>, StudentCompare> pq;
     pq.push(s1);
     pq.push(s2);
     pq.push(s3);
+    pq.push(s4);
+    pq.push(s5);
 
     while(!pq.empty())
     {
