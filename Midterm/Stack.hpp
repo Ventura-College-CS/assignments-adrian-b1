3990f0551d745e08ad44bcbd5c07a7a83d35ec38
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 21:53:14 -0800

    midterm q1 progress

--- Midterm/Stack.hpp
+++ Midterm/Stack.hpp
@@ -8,5 +8,22 @@ using namespace std;
 template<class T>
 class Stack
 {
-    
+    private:
+        vector<T> pool;
+    public:
+        Stack();
+        void push(const T &el)
+        {
+            pool.push_back(el);
+        }
+        T pop()
+        {
+           T el = pool.back();
+           pool.pop_back();
+           return el; 
+        }
+        T &topEl()
+        {
+            
+        }
 }
