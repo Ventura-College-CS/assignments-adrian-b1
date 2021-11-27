eb17f9ee47306dfaa8ccc419045d88a2a30d39b3
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-11-26 21:15:06 -0800

    midterm q1 progress

--- Midterm/Student.hpp
+++ Midterm/Student.hpp
@@ -13,4 +13,21 @@ class Student
         string sName;
         double scores[3];
     public:
+    Student();
+    Student(int i, string name, double *sc)
+    {
+        sID=i;
+        name=sName;
+        for(int i=0;i<3;i++)
+            scores[i]= *(sc+i);
+    }
+    int getID()
+    {
+        return sID;
+    }
+    string getName()
+    {
+        return sName;
+    }
+    
 }
