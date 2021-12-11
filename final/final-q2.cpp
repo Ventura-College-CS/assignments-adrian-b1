9f9ae75a11555689d6b0e4197c91d65ec60dfd87
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-12-10 23:58:15 -0800

    final q2 progress

--- final/final-q2.cpp
+++ final/final-q2.cpp
@@ -41,6 +41,18 @@ class Course
             return cred;
         }
 };
+int partition(Course array[], int first, int last)
+{
+    int pivot = array[last].getID();
+    int i = -1;
+    for (int j=0; j<last;j++)
+    {
+        if (array[j].getID<pivot)
+            swap(array[++i], array[j]);
+    }
+    swap(array[i+1],array[last]);
+    return i+1;
+}
 
 int main()
 {
