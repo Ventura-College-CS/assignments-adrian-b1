6bc7ab7fc2316e13f6109b21afd6b208c47ba9c7
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-12-11 01:19:25 -0800

    final q3 progress

--- final/final-q3.cpp
+++ final/final-q3.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <queue>
 
+using namespace std;
 int main()
 {
     char input[25];
-    
+    cout<<"Please input the postfix expression to calculate in 25 characters or less."<<endl;
+    cin>>input;
+    int queue[25];
+    for (int i=0;i<25;i++)
+    {
+        if(input[i]==('0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'))
+        {}
+        else
+            {if(input[i]==)
+    }
 }
