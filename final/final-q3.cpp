841d315b5be2ccf3e8f6119762edbbb88fb4253c
adrian-b1 <kestral_bruce1@my.vcccd.edu>
2021-12-11 02:19:29 -0800

    after testing, changed to deque to fix order

--- final/final-q3.cpp
+++ final/final-q3.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
 #include <queue>
-
+#include<deque>
 using namespace std;
 int main()
 {
     char input[25];
     cout<<"Please input the postfix expression to calculate in 25 characters or less."<<endl;
     cin>>input;
-    queue<int> queue;
+    deque<int> queue;
     int a, b;
     for (int i=0;i<25;i++)
     {
         if((input[i]=='0')||(input[i]=='1')||(input[i]=='2')||(input[i]=='3')||(input[i]=='4')||(input[i]=='5')||(input[i]=='6')||(input[i]=='7')||(input[i]=='8')||(input[i]=='9'))
         {switch(input[i]){
             case '0':
-                queue.push(0);
+                queue.push_front(0);
                 break;
             case '1':
-                queue.push(1);
+                queue.push_front(1);
                 break;
             case '2':
-                queue.push(2);
+                queue.push_front(2);
                 break;
             case '3':
-                queue.push(3);
+                queue.push_front(3);
                 break;
             case '4':
-                queue.push(4);
+                queue.push_front(4);
                 break;
             case '5':
-                queue.push(5);
+                queue.push_front(5);
                 break;
             case '6':
-                queue.push(6);
+                queue.push_front(6);
                 break;
             case '7':
-                queue.push(7);
+                queue.push_front(7);
                 break;
             case '8':
-                queue.push(8);
+                queue.push_front(8);
                 break;
             case '9':
-                queue.push(9);
+                queue.push_front(9);
                 break;
             }
         }
@@ -50,24 +50,24 @@ int main()
             if((input[i]=='+')||(input[i]=='*')||(input[i]=='/')||(input[i]=='-')||(input[i]=='%'))
             {
                 a=queue.front();
-                queue.pop();
+                queue.pop_front();
                 b=queue.front();
-                queue.pop();
+                queue.pop_front();
                  switch(input[i]){
                     case '*':
-                        queue.push(a*b);
+                        queue.push_front(b*a);
                         break;
                     case '+':
-                        queue.push(a+b);
+                        queue.push_front(b+a);
                         break;
                     case '/':
-                        queue.push(a/b);
+                        queue.push_front(b/a);
                         break;
                     case '-':
-                        queue.push(a-b);
+                        queue.push_front(b-a);
                         break;
                     case '%':
-                        queue.push(a%b);
+                        queue.push_front(b%a);
                         break;
                     }
                 }
